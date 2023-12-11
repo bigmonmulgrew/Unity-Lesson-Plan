7bb106be4151fff79051bd8dc56f0fbe2c917b06
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2023-12-11 20:06:14 +0000

    Made Hello World Game

---
+++ Lesson 3/Hello World/Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    string myString = "Hello David";
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World");
+        Debug.Log(myString);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
