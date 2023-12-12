5f6ebd8bc73faab1a840f142191561cb1ce701a8
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2023-12-12 13:14:09 +0000

    Finished Lesson 4

--- Lesson 4 Number Wizard/Number Wizard/Assets/Scripts/Wizard.cs
+++ Lesson 4 Number Wizard/Number Wizard/Assets/Scripts/Wizard.cs
@@ -8,14 +8,15 @@ public class Wizard : MonoBehaviour
     [SerializeField] TMPro.TMP_Text textBox;
 
     int min = 1;
-    int max = 10;
+    int max = 11;   // Random.Range is exclusive so we need 1 higher than our range
     int guess;
     int attempts = 3;
 
     // Start is called before the first frame update
     void Start()
     {
-        guess = Random.Range(min, max + 1);
+        // Random.Range is inclusive of min but exclusive of max
+        guess = Random.Range(min, max);
 
         textBox.text = $"My guess is \n\n {guess}";
     }
@@ -23,6 +24,48 @@ public class Wizard : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        CheckInput();
+    }
+
+    void CheckInput()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            min = guess + 1;
+            NextGuess();
+        }
+        else if (Input.GetKeyDown(KeyCode.L))
+        {
+            max = guess;
+            NextGuess();
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            WinGame();
+        }
+    }
+
+    void NextGuess()
+    {
+        attempts--;
+        if (attempts <= 0)
+        {
+            LoseGame();
+            return;
+        }
+
+        guess = Random.Range(min, max);
+
+        textBox.text = $"My guess is \n\n {guess}";
+    }
+
+    void WinGame()
+    {
+        textBox.text = $"I guessed it!\n\nYour number was {guess}";
+    }
+
+    void LoseGame()
+    {
+        textBox.text = $"I ran out of attempts.\n\nYou win!";
     }
 }
