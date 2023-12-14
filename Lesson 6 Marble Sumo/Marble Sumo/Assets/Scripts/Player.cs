a7cf5a3738eb71494b3ec1848140042e8a49d852
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2023-12-14 08:50:43 +0000

    Add player script

--- Lesson 6 Marble Sumo/Marble Sumo/Assets/Scripts/Player.cs
+++ Lesson 6 Marble Sumo/Marble Sumo/Assets/Scripts/Player.cs
@@ -1,18 +1,83 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Configurations
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float jumpForce = 7f;
+    [SerializeField] float groundPoundForce = 10f;
+    [SerializeField] GameObject platform;
+
+    //Cached references
+    Rigidbody rb;
+
+    // State Variables
+    bool isGrounded;
+    bool pound;
+    int jumpCount = 0;
+
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    //Update is called each frame
+    void Update()
+    {
+        Jump();
+    }
+
+    //Fixed update is triggered each Physics update Usually 60fps
+    void FixedUpdate()
+    {   
+        // We are using force to move rather than fixed movement
+        Move();
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 forceToAdd = new Vector3(x, 0f, z) * moveSpeed;
+        rb.AddForce(forceToAdd);
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGrounded || jumpCount < 2)
+            {
+                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                isGrounded = false;
+                jumpCount++;
+                               
+            }
+            else if (!isGrounded && jumpCount > 0) // Ground Pound
+            {
+                float calculatedForce = groundPoundForce * (transform.position.y - platform.transform.position.y);
+                rb.AddForce(Vector3.down * calculatedForce, ForceMode.Impulse);
+                pound = true;
+            }
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnCollisionEnter(Collision collision)
     {
-        
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            if(pound) Knockback();
+            isGrounded = true;
+            jumpCount = 0;
+        }
+    }
+
+    void Knockback()
+    {
+        throw new NotImplementedException();
     }
 }
